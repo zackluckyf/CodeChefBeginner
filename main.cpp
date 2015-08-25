98d5a23709c1b411f1319b61bea683e2ae93a25a
zackluckyf <fanning.34@gmail.com>
2015-08-25 12:01:48 -0400

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  newtest
+//
+//  Created by Zack Fanning on 8/25/15.
+//  Copyright (c) 2015 Zackluckyf. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    for (int i=0; i < 10; i++)
+    {
+     std::cout << i << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
