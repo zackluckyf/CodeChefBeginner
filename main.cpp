5785061e881f5a1d8c5118e78799b80eceb876cb
zackluckyf <fanning.34@gmail.com>
2015-08-25 13:42:31 -0400

    made a file that tells you what day of the week 01/01/XXXX is

--- main.cpp
+++ main.cpp
@@ -9,10 +9,36 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    for (int i=0; i < 10; i++)
+    // initiallize variables
+    int year(0), i(1);
+    // get user input
+    std::cout << "Enter a year between 1900 and 2500" << std::endl;
+    // input variable
+    std::cin >> year;
+    // 1/1/1900 was a monday
+    for(int j = 1900; j < year + 1; j++)
     {
-     std::cout << i << " ";
+        if (j % 400 == 0)
+        {
+            i+=2;
+        }
+        else if (j % 100 == 0)
+        {
+            //do nothing
+        }
+        else if (j % 4 == 0)
+        {
+            i+=2;
+        }
+        else
+        {
+            i++;
+        }
     }
+    int k = i % 7;
+    std::string day[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    
+    std::cout << day[k];
     std::cout << std::endl;
     return 0;
 }
